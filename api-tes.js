799ef12f26b4148b74f6c2abed79c2439e7e1c51
60556224-cyber <60556224@continental.edu.pe>
2025-09-25 15:03:38 -0500

    Update api-tes.js

--- api-tes.js
+++ api-tes.js
@@ -1,10 +1,1 @@
-const claveApi = "a51f05b8b5494319add222836252109"; 
-const idioma = "es-ES";
-const cuidad = "huancayo";
-
-const apiclimaactual = 'http://api.weatherapi.com/v1/current.json?key=a51f05b8b5494319add222836252109&q=huancayo&aqi=no'
-
-const response = await fetch(apiclimaactual);
-let data = await response.json();
-
-console.log(data);
+const claveApi = "b1b15e88fa797225412429c1c50c122a1"; // Reemplaza con tu clave de API de OpenWeatherMap
